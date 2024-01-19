bda65a13a383017e66f8a15f66ff5a675ee15759
goziyev <goziyevmuhammadgozi@gmail.com>
2024-01-19 23:27:56 +0500

    task completed

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-localStorage.setItem("Name", "John");
+let fruits = ['Olma','Anor','Behi','Nok','Xurmo','Uzum','Olcha'];
+const wrapper = document.getElementById('wrapper') 
+
+localStorage.setItem("Name", JSON.stringify(fruits));
 localStorage.setItem("Father", "Smit");
 
 sessionStorage.setItem("user", JSON.stringify([{name:"ali",username:"adminus",age:34}]));
@@ -10,4 +13,20 @@ document.cookie = "cookie=cookie@gmail.com";
 
 indexedDB.open('Hello world',1)
 indexedDB.open('Bye ',1)
+
+function createItem(el){
+    let li = document.createElement('li');
+    li.innerHTML = el;
+    return li;
+}
+ 
+const frag = document.createDocumentFragment();
+ 
+fruits.forEach(el => {
+    let li  = createItem(el);
+    frag.appendChild(li);
+})
+// console.log(frag);
+wrapper.appendChild(frag)
+
 
